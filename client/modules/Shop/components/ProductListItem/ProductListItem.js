adb05fea78f89c483cc03fea4c05cf0064339bde
Arien Kock <arien@positor.nl>
2018-02-05 10:25:36 +0100

    Translated labels

--- client/modules/Shop/components/ProductListItem/ProductListItem.js
+++ client/modules/Shop/components/ProductListItem/ProductListItem.js
@@ -1,11 +1,12 @@
 import React, { PropTypes } from 'react';
+import { FormattedMessage } from 'react-intl';
 
 function ProductListItem(props, context) {
   return (
     <div style={{marginBottom: '10px'}}>
       <ul>
-        <li>Name: {props.name}</li>
-        <li>Date added: {props.dateAdded}</li>
+        <li><FormattedMessage id="productName"/>: {props.name}</li>
+        <li><FormattedMessage id="dateAdded"/>: {props.dateAdded}</li>
       </ul>
     </div>
   );
